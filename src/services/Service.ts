16dd31c0ebe7079e85e96ba5515c140a4e7ab854
Garrett Crowe <garrett.crowe@marriott.com>
2024-05-17 08:39:18 -0500

    update axios

--- src/services/Service.ts
+++ src/services/Service.ts
@@ -1,13 +1,19 @@
-import Guest from "../model/Guest";
+import axios from "axios";
+import Guest, { Occasion } from "../model/Guest";
 
 export default class Service {
     static async createGuest(): Promise<Guest> {
-        const response = await fetch("https://asdtebzbufpfwvgsaylv.supabase.co/rest/v1/Member", {
-            method: 'POST',
-            headers: {
-                'apikey': 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImFzZHRlYnpidWZwZnd2Z3NheWx2Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3MTU4NzE5ODQsImV4cCI6MjAzMTQ0Nzk4NH0.R1ShT_PqQZZ8jKgNQSXSS6v--tVtfblKHtoaVLcQH3g'
-            }
-        }).catch((err) => console.error(err))
-        return await response.json() as Guest
+        return await (await axios.post('Member')).data
+    }
+
+    static async createOcassion(): Promise<Occasion> {
+        return await (await axios.post('Member_Occasion')).data
     }
 }
+
+export const axiosInstance = axios.create({
+    baseURL: 'https://asdtebzbufpfwvgsaylv.supabase.co/rest/v1/',
+    headers: {
+        'apikey': 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImFzZHRlYnpidWZwZnd2Z3NheWx2Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3MTU4NzE5ODQsImV4cCI6MjAzMTQ0Nzk4NH0.R1ShT_PqQZZ8jKgNQSXSS6v--tVtfblKHtoaVLcQH3g'
+    },
+}) ;
